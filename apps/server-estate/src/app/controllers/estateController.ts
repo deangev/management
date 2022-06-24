1686de0f1068d12d9defa91f8aad3772a334e818
greenbackboogie97 <greenbackboogie@tutamail.com>
2022-06-24 12:12:55 +0300

    create CRUD controllers

--- apps/server-estate/src/app/controllers/estateController.ts
+++ apps/server-estate/src/app/controllers/estateController.ts
@@ -1,10 +1,47 @@
-import { catchAsync } from '@sagi/core/utils';
-import { Response } from 'express';
+import { catchAsync, restrictUpdate } from '@sagi/core/utils';
+import { Response, Request } from 'express';
+import Estate from '../models/EstateModel'
+import { Estate as EstateType } from '@sagi/core/types'
 
-const getEstates = catchAsync(async (req: Request, res: Response) => {
-  // const estatesRes = await Estate
+interface RDbyIDRequestType extends Request {
+  params: { id: EstateType['_id'] }
+}
+interface CreateEstateRequestType extends Request {
+  body: EstateType
+}
 
-  res.status(200).json([]);
+interface EditEstateRequestType extends Request {
+  params: { id: EstateType['_id'] }
+  body: EstateType
+}
+
+const getEstate = catchAsync(async (req: RDbyIDRequestType, res: Response) => {
+  const { id: estateID } = req.params
+  const estate = await Estate.findById(estateID)
+  if (!estate) return res.status(400).json({ message: 'estate not found' })
+  res.status(200).json({ estate })
 });
 
-export { getEstates };
+const createEstate = catchAsync(async (req: CreateEstateRequestType, res: Response): Promise<void> => {
+  const { address, apartments, floors } = req.body
+  if (!address) res.status(400).json({ message: 'please provide an address' })
+  if (!apartments) res.status(400).json({ message: 'please provide an address' })
+  if (!floors) res.status(400).json({ message: 'please provide an address' })
+
+  const newEstate = await Estate.create({ address, apartments, floors })
+  res.status(201).json({ estate: newEstate })
+})
+
+const deleteEstate = catchAsync(async (req: RDbyIDRequestType, res: Response): Promise<void> => {
+  const { id: estateID } = req.params
+  await Estate.findByIdAndDelete(estateID)
+  res.status(204).json()
+})
+
+const updateEstate = catchAsync(async (req: EditEstateRequestType, res: Response): Promise<void> => {
+  const { id: estateID } = req.params
+  const updatedEstate = await Estate.findByIdAndUpdate(estateID, restrictUpdate({ ...req.body }, ['contacts', 'address']), { new: true, runValidators: true })
+  res.status(200).json({ updatedEstate })
+})
+
+export { getEstate, createEstate, deleteEstate, updateEstate };
--- apps/server-estate/src/app/routers/estateRouter.ts
+++ apps/server-estate/src/app/routers/estateRouter.ts
@@ -1,7 +1,11 @@
-import { auth } from '@sagi/core/middleware';
-import * as express from 'express';
-const router = express.Router();
+import { Router } from 'express'
+import { createEstate, deleteEstate, getEstate, updateEstate } from '../controllers/estateController'
+const estateRouter = Router()
 
-// router.get('/', getEstates);
+estateRouter.get('/:id', getEstate)
+estateRouter.get('/search', () => {})
+estateRouter.put('/:id', updateEstate)
+estateRouter.delete('/:id', deleteEstate)
+estateRouter.post('/', createEstate)
 
-export default router;
+export default estateRouter;
