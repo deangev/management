bd3f39df3596f7a7ada4e4beef2d46379608a922
greenbackboogie97 <greenbackboogie@tutamail.com>
2022-06-25 00:26:27 +0300

    fixed pr comment

--- libs/core/utils/src/lib/restrictUpdate.ts
+++ libs/core/utils/src/lib/restrictUpdate.ts
@@ -1,4 +1,4 @@
-export function restrictUpdate(body: Record<string ,unknown>, allowedFields: Array<keyof typeof body>) {
+export function restrictUpdate(body: { [key: string]: unknown }, allowedFields: Array<keyof typeof body>) {
     Object.keys(body).forEach(key => !allowedFields.includes(key) && delete body[key]);
     return body;
 };
