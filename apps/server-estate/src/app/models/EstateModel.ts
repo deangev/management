e5c08c0a641efc68a67ee9db04566613975873ad
greenbackboogie97 <greenbackboogie@tutamail.com>
2022-06-25 00:26:10 +0300

    removed sub-document _id's, orginized and fixed minor issues

--- apps/server-estate/src/app/models/EstateModel.ts
+++ apps/server-estate/src/app/models/EstateModel.ts
@@ -1,51 +1,33 @@
 import { model, Schema } from "mongoose"
-import { Estate as EstateType } from '@sagi/core/types'
-import isMobilePhone from 'validator/es/lib/isMobilePhone'
+import validator from 'validator'
+import { EstateType } from '@sagi/core/types'
 
 const estateSchema = new Schema<EstateType>({
     address: {
         type: {
-            city: {
-                type: String,
-                required: [true, 'city is a required value']
-            },
-            street: {
-                type: String,
-                required: [true, 'street is a required value'],
-            },
-            number: {
-                type: Number,
-                required: [true, 'house number is a required value']
-            },
+            city: { type: String, required: [true, 'city is a required value'] },
+            street: { type: String, required: [true, 'street is a required value'] },
+            number: { type: Number, required: [true, 'house number is a required value'] },
             entry: Number,
         },
-        unique: true
+        required: true,
+        _id: false,
     },
-    floors: {
-        type: Number,
-        required: [true, 'please state number of floors']
-    },
-    apartments: {
-        type: Number,
-        required: [true, 'please state number of apartments']
-    },
+    floors: { type: Number, required: [true, 'please state number of floors'] },
+    apartments: { type: Number, required: [true, 'please state number of apartments'] },
     contacts: [{
         type: {
-            name: {
-                type: String,
-                required: [true, 'contact must have a name']
-            },
+            name: { type: String, required: [true, 'contact must have a name'] },
             phoneNumber: {
-                type: String,
-                required: [true, 'contact must have a phone number'],
-                validate: {
-                    validator: function(value: string) {
-                        return isMobilePhone(value, ['he-IL'])
+                type: String, required: [true, 'contact must have a phone number'],
+                validate: { validator: (value: string) => {
+                        return validator.isMobilePhone(value, ['he-IL'])
                     },
                     message: 'phone number is invalid',
                 }
             }
-        }
+        },
+        _id: false
     }]
 })
 
