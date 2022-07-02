44fc217dea11f829e2a00539bc560e421567bc3c
Dean Geva <dean.geva@tikalk.com>
2022-07-02 20:56:32 +0300

    integrated with the service call page

--- libs/pages/service-calls/src/lib/serviceCall/ServiceCall.tsx
+++ libs/pages/service-calls/src/lib/serviceCall/ServiceCall.tsx
@@ -3,13 +3,14 @@ import { useQuery } from '@apollo/client';
 import { ServiceCallType } from '@management/core/types';
 import { ServiceCallQuery } from '@management/graphql-services';
 
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 /* eslint-disable-next-line */
 export interface ServiceCallProps {
   route: {
     params: { serviceCallID: string };
-  }
+  };
 }
 
 interface QueryType {
@@ -17,12 +18,22 @@ interface QueryType {
 }
 
 export function ServiceCall(props: ServiceCallProps) {
+  const navigation = useNavigation();
   const { data } = useQuery<QueryType>(ServiceCallQuery, {
-    variables: { serviceCallID: props.route.params.serviceCallID }
-  })
+    variables: { serviceCallID: props.route.params.serviceCallID },
+  });
+
+  const handleEditPress = () => {
+    //@ts-ignore
+    navigation.navigate('service-call-create-form', {
+      serviceCall: data?.serviceCallData,
+    });
+  };
 
   return (
     <View>
+      <Button onPress={handleEditPress} title="ערוך בניין" />
+
       <Text>Welcome to serviceCall!</Text>
       <Text>{props.route.params.serviceCallID}</Text>
       <Text>{data?.serviceCallData.type}</Text>
