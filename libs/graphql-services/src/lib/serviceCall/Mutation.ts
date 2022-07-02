3d448fee53de21c661ce393dbf3cca3a19006c9f
Dean Geva <dean.geva@tikalk.com>
2022-07-02 12:07:12 +0300

    create service call fix

--- libs/graphql-services/src/lib/serviceCall/Mutation.ts
+++ libs/graphql-services/src/lib/serviceCall/Mutation.ts
@@ -3,14 +3,12 @@ import { gql } from '@apollo/client';
 export const createServiceCallMutation = gql`
   mutation CreateServiceCall(
     $estateId: String!
-    $type: String
     $description: String!
     $destination: String!
   ) {
     createServiceCall(
       serviceCallData: {
         estateId: $estateId
-        type: $type
         description: $description
         destination: $destination
       }
@@ -25,8 +23,6 @@ export const createServiceCallMutation = gql`
       note
       type
       images
-      updatedAt
-      createdAt
     }
   }
 `;
