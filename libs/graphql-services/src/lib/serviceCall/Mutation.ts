05fef8e648db7b3cda1522901a351e1c6d5fe972
greenbackboogie97 <greenbackboogie@tutamail.com>
2022-07-02 14:56:52 +0300

    added fields to mutation

--- libs/graphql-services/src/lib/serviceCall/Mutation.ts
+++ libs/graphql-services/src/lib/serviceCall/Mutation.ts
@@ -3,14 +3,26 @@ import { gql } from '@apollo/client';
 export const createServiceCallMutation = gql`
   mutation CreateServiceCall(
     $estateId: String!
+    $apartment: Int
     $description: String!
     $destination: String!
+    $priority: String
+    $assignee: String
+    $note: String
+    $type: String
+    $images: [String!]
   ) {
     createServiceCall(
       serviceCallData: {
         estateId: $estateId
+        apartment: $apartment
         description: $description
         destination: $destination
+        priority: $priority
+        assignee: $assignee
+        note: $note
+        type: $type
+        images: $images
       }
     ) {
       _id
